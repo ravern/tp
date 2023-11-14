efec1705c3b49da139fec9a8d49f36ad301a08e4
Ravern Koh <ravernkoh@gmail.com>
2023-11-14 09:46:31 +0800

    Print correct app name

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -176,7 +176,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        logger.info("Starting AddressBook " + MainApp.VERSION);
+        logger.info("Starting TAfinder " + MainApp.VERSION);
         ui.start(primaryStage);
     }
 
